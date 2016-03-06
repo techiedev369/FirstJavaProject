edc574c1b041ba6cf61f5add0fcac1aa698ae16d
Devendra <smartdev369@outlook.com>
2016-03-06 09:55:32 -0600

    First Change.

--- First/src/firstJavaProject.java
+++ First/src/firstJavaProject.java
@@ -15,6 +15,7 @@ System.out.println("This is my first java program.");
 System.out.println(new DecimalFormat("##.##").format(d));
 System.out.println("\nmy name is Devendra Pandey");
 System.out.println("This is git.");
+System.out.println("Test 1");
 //Second sec=new Second();
 //sec.display();
 
